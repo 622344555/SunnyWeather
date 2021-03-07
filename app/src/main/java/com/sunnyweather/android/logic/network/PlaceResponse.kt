e0318579aaa84167b680960302d4e3beb4f3b771
yyq2014yyq <280639138@qq.com>
2021-03-07 16:04:53 +0800

    加入显示天气信息的功能

--- app/src/main/java/com/sunnyweather/android/logic/network/PlaceResponse.kt
+++ app/src/main/java/com/sunnyweather/android/logic/network/PlaceResponse.kt
@@ -6,7 +6,7 @@ import com.sunnyweather.android.logic.model.PlaceResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-interface PlaceService {
+interface PlaceResponse {
     @GET("v2/place?token=${SunnyWeatherApplication.TOKEN}&lang=zh_CN")
     fun searchPlaces(@Query("query") query: String): Call<PlaceResponse>
 }
